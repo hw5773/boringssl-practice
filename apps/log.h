d115fd6a48bf570fa8a0d0467289b0ee129282a1
Hyunwoo Lee <hwlee2014@mmlab.snu.ac.kr>
2019-02-22 10:02:38 +0900

    applications

---
+++ apps/log.h
@@ -0,0 +1,6 @@
+#ifndef __PRACTICE_LOG_H__
+#define __PRACTICE_LOG_H__
+
+#define DEBUG_MSG(format, ...) printf("[boringssl-practice] " format "\n", ## __VA_ARGS__)
+
+#endif /* __PRACTICE_LOG_H__ */
